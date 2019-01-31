29d427047dec37c3234c59d7b6b9fadb8e63cb40
Oskar Kristiansen <skakkristiansen@gmail.com>
2019-01-31 20:42:47 +0100

    Layout for Transport + Supplier on client side

---
+++ LogisticsBooking.FrontEnd/Pages/Client/Transporters/Transporter_Create.cshtml.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace LogisticsBooking.FrontEnd.Pages.Client.Transporters
+{
+    public class Transporter_CreateModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
