01f4dcfa39849acf6d9f86ec89625af021a49357
Oskar Kristiansen <skakkristiansen@gmail.com>
2019-01-31 11:37:26 +0100

    Tested and fixed Exception Manual Logging and added to gitignore

--- LogisticsBooking.FrontEnd/Pages/Index.cshtml.cs
+++ LogisticsBooking.FrontEnd/Pages/Index.cshtml.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using LogisticsBooking.FrontEnd.DataServices;
+using LogisticsBooking.FrontEnd.Utilities;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
--- LogisticsBooking.FrontEnd/Utilities/ExceptionUtility.cs
+++ LogisticsBooking.FrontEnd/Utilities/ExceptionUtility.cs
@@ -18,36 +18,38 @@ namespace LogisticsBooking.FrontEnd.Utilities
         {
             // Include logic for logging exceptions
             // Get the absolute path to the log file
-            string logFile = "~/App_Data/ErrorLog.txt";
+            string logFile = "Utilities/ExceptionLog.txt";
 
             // Open the log file for append and write the log
-            StreamWriter sw = new StreamWriter(logFile, true);
-            sw.WriteLine("********** {0} **********", DateTime.Now);
-            if (exc.InnerException != null)
+            using (StreamWriter sw = new StreamWriter(logFile, true))
             {
-                sw.Write("Inner Exception Type: ");
-                sw.WriteLine(exc.InnerException.GetType().ToString());
-                sw.Write("Inner Exception: ");
-                sw.WriteLine(exc.InnerException.Message);
-                sw.Write("Inner Source: ");
-                sw.WriteLine(exc.InnerException.Source);
-                if (exc.InnerException.StackTrace != null)
+                sw.WriteLine("********** {0} **********", DateTime.Now);
+                if (exc.InnerException != null)
                 {
-                    sw.WriteLine("Inner Stack Trace: ");
-                    sw.WriteLine(exc.InnerException.StackTrace);
+                    sw.Write("Inner Exception Type: ");
+                    sw.WriteLine(exc.InnerException.GetType().ToString());
+                    sw.Write("Inner Exception: ");
+                    sw.WriteLine(exc.InnerException.Message);
+                    sw.Write("Inner Source: ");
+                    sw.WriteLine(exc.InnerException.Source);
+                    if (exc.InnerException.StackTrace != null)
+                    {
+                        sw.WriteLine("Inner Stack Trace: ");
+                        sw.WriteLine(exc.InnerException.StackTrace);
+                    }
                 }
+                sw.Write("Exception Type: ");
+                sw.WriteLine(exc.GetType().ToString());
+                sw.WriteLine("Exception: " + exc.Message);
+                sw.WriteLine("Source: " + source);
+                sw.WriteLine("Stack Trace: ");
+                if (exc.StackTrace != null)
+                {
+                    sw.WriteLine(exc.StackTrace);
+                    sw.WriteLine();
+                }
+                sw.Close();
             }
-            sw.Write("Exception Type: ");
-            sw.WriteLine(exc.GetType().ToString());
-            sw.WriteLine("Exception: " + exc.Message);
-            sw.WriteLine("Source: " + source);
-            sw.WriteLine("Stack Trace: ");
-            if (exc.StackTrace != null)
-            {
-                sw.WriteLine(exc.StackTrace);
-                sw.WriteLine();
-            }
-            sw.Close();
         }
     }
 }
