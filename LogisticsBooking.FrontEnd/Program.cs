b4fe0e1c52247caf8991e95a3dc920f5a1a2627c
Casper Hansern <caspha17@gmail.com>
2019-04-23 12:11:03 +0200

    added https kestrel

--- LogisticsBooking.FrontEnd/Program.cs
+++ LogisticsBooking.FrontEnd/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Net;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
@@ -18,10 +19,19 @@ namespace LogisticsBooking.FrontEnd
         }
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
-        
+
             WebHost.CreateDefaultBuilder(args)
-               
+
+                .UseKestrel(options =>
+                {
+                    options.Listen(IPAddress.Loopback, 5000); // http:localhost:5000
+                    options.Listen(IPAddress.Any, 80); // http:*:80
+                    options.Listen(IPAddress.Loopback, 443,
+                        listenOptions => { listenOptions.UseHttps("localhost.pfx", "password"); });
+                })
                 .UseStartup<Startup>();
-            
+
+
+
     }
 }
