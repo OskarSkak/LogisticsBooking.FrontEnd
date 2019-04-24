119d59a36583c7a29fcf1292ff1c905397ed951c
Casper Hansern <caspha17@gmail.com>
2019-04-24 17:23:34 +0200

    df

--- LogisticsBooking.FrontEnd/Program.cs
+++ LogisticsBooking.FrontEnd/Program.cs
@@ -11,25 +11,33 @@ using Microsoft.Extensions.Logging;
 
 namespace LogisticsBooking.FrontEnd
 {
-        public class Program
+    public class Program
+    {
+
+        public static IHostingEnvironment HostingEnvironment { get; set; }
+        public static IConfiguration Configuration { get; set; }
+
+        public static void Main(string[] args)
         {
-            
-            public static IHostingEnvironment HostingEnvironment { get; set; }
-            public static IConfiguration Configuration { get; set; }
-            public static void Main(string[] args)
-            {
             CreateWebHostBuilder(args).Build().Run();
         }
 
-        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args)
+        {
+            var config = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", optional: false)
+                .Build();
 
-            WebHost.CreateDefaultBuilder(args)
+    return WebHost.CreateDefaultBuilder(args)
+        .UseStartup<Startup>()
+        .ConfigureKestrel((context, options) =>
+        {
+            
+        });
 
-                .UseKestrel()
-                
-                .UseStartup<Startup>();
 
+}
 
-
-    }
+}
 }
