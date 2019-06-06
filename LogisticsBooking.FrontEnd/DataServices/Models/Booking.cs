d350e0120c14897a834aa22156bb5d42ef761b96
Casper Hansern <caspha17@gmail.com>
2019-06-06 10:57:50 +0200

    Added date format

--- LogisticsBooking.FrontEnd/DataServices/Models/Booking.cs
+++ LogisticsBooking.FrontEnd/DataServices/Models/Booking.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -12,6 +13,8 @@ namespace LogisticsBooking.FrontEnd.DataServices.Models
         public DateTime bookingTime { get; set; }
         public string transporterName { get; set; }
         public int port { get; set; }
+        
+        
         public DateTime actualArrival { get; set; }
         public DateTime startLoading { get; set; }
         public DateTime endLoading { get; set; }
