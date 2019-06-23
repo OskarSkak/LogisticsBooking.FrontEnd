7160532fed7eafc2fe8f0f3d012b2690109d7c22
Casper Hansern <caspha17@gmail.com>
2019-06-23 21:56:28 +0200

    Added new razorpages

---
+++ LogisticsBooking.FrontEnd/Pages/Client/Schedule/CreateNewSchedule.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace LogisticsBooking.FrontEnd.Pages.Client.Schedule
+{
+    public class CreateNewSchedule : PageModel
+    {
+        public void OnGet()
+        {
+            
+        }
+    }
+}
