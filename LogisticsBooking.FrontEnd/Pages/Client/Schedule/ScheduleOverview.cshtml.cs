9988986de4ffce2f296108bfd39603b46d756169
Oskar Kristiansen <skakkristiansen@gmail.com>
2019-06-23 21:50:38 +0200

    Created schedule overview

---
+++ LogisticsBooking.FrontEnd/Pages/Client/Schedule/ScheduleOverview.cshtml.cs
@@ -0,0 +1,15 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace LogisticsBooking.FrontEnd.Pages.Client.Schedule
+{
+    
+    public class ScheduleOverviewIndexModel : PageModel
+    {
+        public void OnGet()
+        {
+            var la = "";
+        }
+
+        
+    }
+}
