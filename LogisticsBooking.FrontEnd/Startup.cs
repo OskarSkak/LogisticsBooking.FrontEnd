ef8e540e93ce4773f5ea56f9c5514ffac62d58ad
Oskar Kristiansen <skakkristiansen@gmail.com>
2019-07-20 23:02:50 +0200

    Removed navbar from error page : UPDATE

--- LogisticsBooking.FrontEnd/Startup.cs
+++ LogisticsBooking.FrontEnd/Startup.cs
@@ -165,6 +165,7 @@ namespace LogisticsBooking.FrontEnd
             }
             else
             {
+                //ON exception trown (Whilst not in dev mode) redirect to this page:
                 app.UseExceptionHandler("/Error");
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
