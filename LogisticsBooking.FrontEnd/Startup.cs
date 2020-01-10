b00bba038b778c6fc4759cbf1da89ab18341e4f1
Casper Hansern <caspha17@gmail.com>
2020-01-10 12:20:32 +0100

    Updated logging to warnings

--- LogisticsBooking.FrontEnd/Startup.cs
+++ LogisticsBooking.FrontEnd/Startup.cs
@@ -68,6 +68,7 @@ namespace LogisticsBooking.FrontEnd
             
             Log.Logger = new LoggerConfiguration()
                 .Enrich.FromLogContext()
+                .MinimumLevel.Warning()
                 .WriteTo.Elasticsearch(new ElasticsearchSinkOptions(new Uri("http://localhost:9200"))
                 {
                     AutoRegisterTemplate = true
