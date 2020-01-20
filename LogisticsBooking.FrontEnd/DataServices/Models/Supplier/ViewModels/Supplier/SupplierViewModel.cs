b50dcdafb0c6154f8eaa8693dcf5bfb1702ebeca
Casper Hansern <caspha17@gmail.com>
2020-01-20 14:14:28 +0100

    test for speed without api

--- LogisticsBooking.FrontEnd/DataServices/Models/Supplier/ViewModels/Supplier/SupplierViewModel.cs
+++ LogisticsBooking.FrontEnd/DataServices/Models/Supplier/ViewModels/Supplier/SupplierViewModel.cs
@@ -1,9 +1,11 @@
 using System;
 using System.ComponentModel.DataAnnotations;
+using AutoMapper;
+using LogisticsBooking.FrontEnd.Acquaintance.Interfaces;
 
 namespace LogisticsBooking.FrontEnd.DataServices.Models.Supplier.Supplier
 {
-    public class SupplierViewModel
+    public class SupplierViewModel : IHaveCustomMapping
     {
         public string Email { get; set; }
         public int Telephone { get; set; }
@@ -14,5 +16,9 @@ namespace LogisticsBooking.FrontEnd.DataServices.Models.Supplier.Supplier
         // Leverings vindue for kunden
         public DateTime DeliveryStart { get; set; }
         public DateTime DeliveryEnd { get; set; }
+        public void CreateMappings(Profile configuration)
+        {
+            configuration.CreateMap<LogisticBooking.API.Domain.Entities.Supplier, SupplierViewModel>();
+        }
     }
 }
--- LogisticsBooking.FrontEnd/Pages/Client/Suppliers/Suppliers.cshtml.cs
+++ LogisticsBooking.FrontEnd/Pages/Client/Suppliers/Suppliers.cshtml.cs
@@ -2,17 +2,23 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using AutoMapper;
+using AutoMapper.QueryableExtensions;
 using LogisticsBooking.FrontEnd.Acquaintance;
 using LogisticsBooking.FrontEnd.DataServices.Models;
+using LogisticsBooking.FrontEnd.DataServices.Models.Supplier.Supplier;
 using LogisticsBooking.FrontEnd.DataServices.Models.Supplier.SuppliersList;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.EntityFrameworkCore;
 
 namespace LogisticsBooking.FrontEnd.Pages.Client
 {
     public class SuppliersModel : PageModel
     {
+        private readonly ILogisticBookingApiDatabase _logisticBookingApiDatabase;
+        private readonly IMapper _mapper;
         private readonly ISupplierDataService _supplierDataService;
         
         
@@ -24,20 +30,22 @@ namespace LogisticsBooking.FrontEnd.Pages.Client
         
         public bool ShowResponseMessage  => !String.IsNullOrEmpty(ResponseMessage);
 
-        public SuppliersModel(ISupplierDataService supplierDataService)
+        public SuppliersModel(ILogisticBookingApiDatabase logisticBookingApiDatabase , IMapper mapper)
         {
-            _supplierDataService = supplierDataService;
-            
+            _logisticBookingApiDatabase = logisticBookingApiDatabase;
+            _mapper = mapper;
         }
         
         public async Task<IActionResult> OnGet()
         {
-            SuppliersListView = await PopulateList(_supplierDataService);
-            Console.WriteLine();
-            foreach (var claim in User.Claims)
+            var suppliers = await _logisticBookingApiDatabase.Suppliers
+                .ProjectTo<SupplierViewModel>(_mapper.ConfigurationProvider).ToListAsync();
+            
+            SuppliersListView =  new SuppliersListViewModel
             {
-                Console.WriteLine(claim.Type + " : " + claim.Value);
-            }
+                Suppliers = suppliers
+            };
+           
             return Page();
         }
 
